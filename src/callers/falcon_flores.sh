8d40dc8abb0b0788169f3c5ec5282cccbd2ac16c
Jaume Prats <jprats@lofn.inf.ed.ac.uk>
2023-11-07 17:29:19 +0000

    experiments with nllb and some definite EVAL for falcon

--- src/callers/falcon_flores.sh
+++ src/callers/falcon_flores.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=6,7
+export CUDA_VISIBLE_DEVICES=5,6
 echo $CUDA_VISIBLE_DEVICES
 
-filename_prefix=TEST_infer
+filename_prefix=EVAL_falcon_flores-dev
 
 timestamp=$(date +"%Y%m%d-%H.%M.%S")
 
@@ -10,12 +10,12 @@ python /fs/surtr0/jprats/code/llm-mt-iberian-languages/src/eval_llm_mt.py \
     --filename_prefix $filename_prefix \
     --timestamp $timestamp \
     --batch_size 8 \
-    --num_beams 1 \
-    --max_new_tokens 100 \
-    --num_fewshot 0 \
+    --num_beams 5 \
+    --max_new_tokens 150 \
+    --num_fewshot 1 \
     --template_id simple \
     --src_examples /fs/surtr0/jprats/data/raw/flores200_dataset/devtest/eng_Latn.devtest \
-    --ref_examples /fs/surtr0/jprats/data/raw/flores200_dataset/devtest/spa_Latn.devtest \
+    --ref_examples /fs/surtr0/jprats/data/raw/flores200_dataset/devtest/cat_Latn.devtest \
     /fs/surtr0/jprats/data/raw/flores200_dataset/dev/eng_Latn.dev \
     /fs/surtr0/jprats/data/raw/flores200_dataset/dev/spa_Latn.dev \
     /fs/surtr0/jprats/code/llm-mt-iberian-languages \
--- src/callers/nllb_flores.sh
+++ src/callers/nllb_flores.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=3
-echo $CUDA_VISIBLE_DEVICES
 
-filename_prefix=TEST_nllb_auto
+filename_prefix=nllb_flores
 
 timestamp=$(date +"%Y%m%d-%H.%M.%S")
 
 python /fs/surtr0/jprats/code/llm-mt-iberian-languages/src/eval_nllb_mt.py \
     --filename_prefix $filename_prefix \
     --timestamp $timestamp \
-    --batch_size 1 \
+    --batch_size 2 \
     --max_length 400 \
-    /fs/surtr0/jprats/data/processed/tiny_flores/eng_Latn.dev \
-    /fs/surtr0/jprats/data/processed/tiny_flores/spa_Latn.dev \
+    --device 7 \
+    /fs/surtr0/jprats/data/raw/flores200_dataset/dev/cat_Latn.dev \
+    /fs/surtr0/jprats/data/raw/flores200_dataset/dev/spa_Latn.dev \
     /fs/surtr0/jprats/code/llm-mt-iberian-languages \
     facebook/nllb-200-3.3B 2> /fs/surtr0/jprats/code/llm-mt-iberian-languages/logs/$filename_prefix'_'$timestamp.log
 
