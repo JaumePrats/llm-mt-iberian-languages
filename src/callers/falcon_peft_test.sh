be13380617ada6db8304f62ba893134afc8e6a1b
Jaume Prats <jprats@lofn.inf.ed.ac.uk>
2023-11-09 17:04:31 +0000

    2nd peft test with falcon and guanaco dataset

--- src/callers/falcon_peft_test.sh
+++ src/callers/falcon_peft_test.sh
@@ -4,7 +4,7 @@ echo $CUDA_VISIBLE_DEVICES
 
 export WANDB_ENTITY=jaume-prats-cristia
 export WANDB_PROJECT=falcon_ft_test
-export WANDB_NAME=falcon_peft_test_0
+export WANDB_NAME=falcon_peft_test_1
 #export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512
 
 python /fs/surtr0/jprats/code/llm-mt-iberian-languages/src/falcon_peft_test.py \
