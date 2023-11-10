cb45275ea2cd4fd053c9ae296b89f63038d8cb8c
Jaume Prats <jprats@lofn.inf.ed.ac.uk>
2023-11-10 18:47:28 +0000

    added log file

--- src/callers/falcon_peft_test.sh
+++ src/callers/falcon_peft_test.sh
@@ -2,11 +2,14 @@
 export CUDA_VISIBLE_DEVICES=7
 echo $CUDA_VISIBLE_DEVICES
 
+filename_prefix=falcon_peft_test_2
+
+timestamp=$(date +"%Y%m%d-%H.%M.%S")
 export WANDB_ENTITY=jaume-prats-cristia
 export WANDB_PROJECT=falcon_ft_test
-export WANDB_NAME=falcon_peft_test_1
-#export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512
+export WANDB_NAME=$filename_prefix'_'$timestamp
 
 python /fs/surtr0/jprats/code/llm-mt-iberian-languages/src/falcon_peft_test.py \
     --model_name tiiuae/falcon-7b \
     --dataset_name timdettmers/openassistant-guanaco \
+    2> /fs/surtr0/jprats/code/llm-mt-iberian-languages/logs/$filename_prefix'_'$timestamp.log
