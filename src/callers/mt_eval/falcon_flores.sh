71da613d0287ff2c4330a9e4163f07c24a8be7dc
Jaume Prats <jprats@lofn.inf.ed.ac.uk>
2023-12-04 17:49:55 +0000

    Group by Length Study: mt evaluations for finetuned models with and without group by length enabled

--- src/callers/mt_eval/falcon_flores.sh
+++ src/callers/mt_eval/falcon_flores.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=3
 echo GPU:$CUDA_VISIBLE_DEVICES
 
 model=tiiuae/falcon-7b
-adapter=/fs/surtr0/jprats/models/falcon_peft_test_1.0-slowTokenizer/checkpoint-1000
-eval_set=dev
-example_set=devtest
-filename_prefix=TEST_directory_structure-$eval_set
+adapter=/fs/surtr0/jprats/models/checkpoints/falcon_qlora_europarl10k_NOgbl_ebs16_linear_lr1e-4_20231128-12.59.27/checkpoint-500
+eval_set=devtest
+example_set=dev
+filename_prefix=study-gbl-disabled_$eval_set
 
 src_lang=spa
 tgt_lang=eng
@@ -22,10 +22,11 @@ python /fs/surtr0/jprats/code/llm-mt-iberian-languages/src/eval_llm_mt.py \
     --max_new_tokens 150 \
     --num_fewshot 0 \
     --template_id simple \
+    --adapter $adapter \
     --src_examples /fs/surtr0/jprats/data/raw/flores200_dataset/${example_set}/${src_lang}_Latn.${example_set} \
     --ref_examples /fs/surtr0/jprats/data/raw/flores200_dataset/${example_set}/${tgt_lang}_Latn.${example_set} \
     /fs/surtr0/jprats/data/raw/flores200_dataset/${eval_set}/${src_lang}_Latn.${eval_set} \
     /fs/surtr0/jprats/data/raw/flores200_dataset/${eval_set}/${tgt_lang}_Latn.${eval_set} \
     /fs/surtr0/jprats/code/llm-mt-iberian-languages \
     $model \
-    2> /fs/surtr0/jprats/code/llm-mt-iberian-languages/logs/${filename_prefix}_${src_lang}-${tgt_lang}_${timestamp}.log
+    2> /fs/surtr0/jprats/code/llm-mt-iberian-languages/logs/mt_eval/${filename_prefix}_${src_lang}-${tgt_lang}_${timestamp}.log
