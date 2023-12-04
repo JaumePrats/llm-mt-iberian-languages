7fffa3f5e4961efb78c582edbd645cd3be2b23e7
Jaume Prats <jprats@lofn.inf.ed.ac.uk>
2023-12-04 17:52:46 +0000

    caller for evaluating aguila model on NLU tasks

---
+++ src/callers/nlu_eval/aguila_nlu_eval.sh
@@ -0,0 +1,24 @@
+prefix=XNLI_correcte-ames
+model=projecte-aina/aguila-7b
+tasks=xnli_ca
+num_fewshot=0
+device=5
+
+echo "NLU EVAL --------------------------------"
+echo "model: $model"
+echo "tasks: $tasks"
+echo "num fewhot: $model"
+echo "cuda device: $device"
+echo "-----------------------------------------"
+
+timestamp=$(date +"%Y%m%d-%H.%M.%S")
+
+echo 
+python /fs/surtr0/jprats/code/lm-evaluation-harness/main.py \
+    --model hf-causal \
+    --model_args pretrained=$model,dtype=float16,trust_remote_code=True \
+    --tasks $tasks \
+    --num_fewshot 0 \
+    --device cuda:${device} \
+    > /fs/surtr0/jprats/code/llm-mt-iberian-languages/results/nlu_eval/${prefix}_"${tasks//,/-}"_nshot${num_fewshot}_${timestamp} \
+    # 2> /fs/surtr0/jprats/code/llm-mt-iberian-languages/logs/nlu_eval/${prefix}_"${tasks//,/-}"_nshot${num_fewshot}_${timestamp}
