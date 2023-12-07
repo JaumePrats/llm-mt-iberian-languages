e6f948911aa0b49c4f8c9807d2348eee4f77e83f
Jaume Prats <jprats@lofn.inf.ed.ac.uk>
2023-12-07 18:54:41 +0000

    added merge log file to output directory

--- src/merge_lora.py
+++ src/merge_lora.py
@@ -10,6 +10,8 @@ import argparse
 import torch
 from peft import PeftModel
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import os
+from datetime import datetime
 
 # model = AutoModelForCausalLM.from_pretrained(model_params['model_id'], torch_dtype=torch.bfloat16, device_map='auto', trust_remote_code=True)
 # model = PeftModel.from_pretrained(model, model_params['adapter'], device_map='auto', torch_dtype=torch.bfloat16)
@@ -34,6 +36,16 @@ def apply_lora(base_model, target_model_path, lora_path):
     print(f"Saving the target model to {target_model_path}")
     model.save_pretrained(target_model_path)
     base_tokenizer.save_pretrained(target_model_path)
+
+    log_path = os.path.join(target_model_path, 'merge_log.txt')
+    with open(log_path, 'w') as f:
+        f.write(f'--base-model : {base_model}\n')
+        f.write(f'--lora-path : {lora_path}\n')
+        f.write(f'--target-model-path : {target_model_path}\n')
+        f.write(f'(creation date : {datetime.now()})')
+
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
