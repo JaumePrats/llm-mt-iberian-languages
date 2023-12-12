253ebc844005c1df64c11d932643069c0af20e1a
Jaume Prats <jprats@alvis.inf.ed.ac.uk>
2023-12-12 16:29:26 +0000

    script for running NLU evaluation

---
+++ src/callers/nlu_eval/nlu_eval.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# PARAMS: -----------------------------------
+prefix=FM_falcon
+model=tiiuae/falcon-7b
+num_fewshot=5
+device=2
+# -------------------------------------------
+
+# GETTING MODEL NAME:
+model_name=$model
+# Set the Internal Field Separator (IFS) to space temporarily
+IFS='/'
+read -ra string_array <<< "$model_name"
+# Reset IFS to its default value (whitespace)
+IFS=$' \t\n'
+array_length=${#string_array[@]}
+# Calculate the index of the last element
+last_index=$((array_length - 1))
+# Access the last element
+model_name="${string_array[last_index]}"
+
+# ============================================
+# WNLI:
+
+# tasks=wnli_ca,wnli_en,wnli_es
+
+# timestamp=$(date +"%Y%m%d-%H.%M.%S")
+
+# echo "---------------------------------------------------"
+# echo NLU EVAL: ${prefix}_${tasks//,/-}_${model_name}_nshot${num_fewshot}_${timestamp}
+# echo "---------------------------------------------------"
+# echo "model: $model"
+# echo "tasks: $tasks"
+# echo "num fewhot: $num_fewshot"
+# echo "cuda device: $device"
+# echo "---------------------------------------------------"
+
+# python /fs/surtr0/jprats/code/lm-evaluation-harness/main.py \
+#     --model hf-causal \
+#     --model_args pretrained=$model,dtype=float16,trust_remote_code=True \
+#     --tasks $tasks \
+#     --num_fewshot $num_fewshot \
+#     --device cuda:${device} \
+#     > /fs/surtr0/jprats/code/llm-mt-iberian-languages/results/nlu_eval/${prefix}_${tasks//,/-}_${model_name}_nshot${num_fewshot}_${timestamp} \
+#     2> /fs/surtr0/jprats/code/llm-mt-iberian-languages/logs/nlu_eval/${prefix}_${tasks//,/-}_${model_name}_nshot${num_fewshot}_${timestamp}
+
+# ============================================
+# XQUAD:
+
+tasks=xquad_ca,xquad_en,xquad_es
+
+timestamp=$(date +"%Y%m%d-%H.%M.%S")
+
+echo "---------------------------------------------------"
+echo NLU EVAL: ${prefix}_${tasks//,/-}_${model_name}_nshot${num_fewshot}_${timestamp}
+echo "---------------------------------------------------"
+echo "model: $model"
+echo "tasks: $tasks"
+echo "num fewhot: $num_fewshot"
+echo "cuda device: $device"
+echo "---------------------------------------------------"
+
+python /fs/surtr0/jprats/code/lm-evaluation-harness/main.py \
+    --model hf-causal \
+    --model_args pretrained=$model,dtype=float16,trust_remote_code=True \
+    --tasks $tasks \
+    --num_fewshot $num_fewshot \
+    --device cuda:${device} \
+    > /fs/surtr0/jprats/code/llm-mt-iberian-languages/results/nlu_eval/${prefix}_${tasks//,/-}_${model_name}_nshot${num_fewshot}_${timestamp} \
+    2> /fs/surtr0/jprats/code/llm-mt-iberian-languages/logs/nlu_eval/${prefix}_${tasks//,/-}_${model_name}_nshot${num_fewshot}_${timestamp}
